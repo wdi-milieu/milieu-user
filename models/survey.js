e6f3ad8ee006e07dc580dacb848f9108af3558a0
weiyang wan <weiyangwan@gmail.com>
2016-08-18 18:04:09 +0800

    Amend survey schema. Add virtual attribute to get npsCategory and add timestamp

--- models/survey.js
+++ models/survey.js
@@ -4,10 +4,24 @@ var User = mongoose.model('User');
 
 var surveySchema = new mongoose.Schema({
   brandName: String,
-  question: String,
-  score: Number,
+  brandUsage: Number,
+  npsScore: Number,
+  npsReason: Number,
   user: User.schema
 });
+
+surveySchema.virtual('npsCategory')
+.get(function()  {
+  if ( this.npsScore <= 6 ) {
+    return "Detractors";
+  } else if ( this.npsScore > 6 && this.npsScore < 9 )  {
+    return "Passive";
+  } else if ( this.npsScore > 8 ) {
+    return "Promoters";
+  }
+});
+
+surveySchema.set('timestamps', {});
 
 var Survey = mongoose.model('Survey', surveySchema);
 
