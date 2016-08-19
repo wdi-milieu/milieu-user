eec9a0431aeca4d419b0524849613c7049aaab90
weiyang wan <weiyangwan@gmail.com>
2016-08-19 12:26:46 +0800

    Added survey references in user data for testing

--- app/models/seed_data.js
+++ app/models/seed_data.js
@@ -9,7 +9,17 @@ user1 = (
     ethnicity: 1,
     householdIncome: 1,
     politicalParty: 1,
-    timeOnline: 1
+    timeOnline: 1,
+    survey: [
+      ObjectId("57b66fc6333a9a6dcc38bb70"),
+      ObjectId("57b66fc1333a9a6dcc38bb6f"),
+      ObjectId("57b66fbb333a9a6dcc38bb6e"),
+      ObjectId("57b66fb2333a9a6dcc38bb6d"),
+      ObjectId("57b66f99333a9a6dcc38bb6c"),
+      ObjectId("57b66f93333a9a6dcc38bb6b"),
+      ObjectId("57b66f8b333a9a6dcc38bb6a"),
+      ObjectId("57b66f2c333a9a6dcc38bb69"),
+      ObjectId("57b66f16333a9a6dcc38bb68")]
   },
   {
     firstName: "Hippie",
@@ -21,7 +31,18 @@ user1 = (
     ethnicity: 2,
     householdIncome: 2,
     politicalParty: 2,
-    timeOnline: 2
+    timeOnline: 2,
+    survey:[
+      ObjectId("57b66ff3333a9a6dcc38bb79"),
+      ObjectId("57b66fee333a9a6dcc38bb78"),
+      ObjectId("57b66fea333a9a6dcc38bb77"),
+      ObjectId("57b66fe6333a9a6dcc38bb76"),
+      ObjectId("57b66fe1333a9a6dcc38bb75"),
+      ObjectId("57b66fdb333a9a6dcc38bb74"),
+      ObjectId("57b66fd6333a9a6dcc38bb73"),
+      ObjectId("57b66fd2333a9a6dcc38bb72"),
+      ObjectId("57b66fcc333a9a6dcc38bb71")
+    ]
   },
   {
     firstName: "Yolo",
@@ -33,7 +54,18 @@ user1 = (
     ethnicity: 3,
     householdIncome: 3,
     politicalParty: 1,
-    timeOnline: 3
+    timeOnline: 3,
+    survey:[
+      ObjectId("57b67028333a9a6dcc38bb82"),
+      ObjectId("57b6701d333a9a6dcc38bb81"),
+      ObjectId("57b67019333a9a6dcc38bb80"),
+      ObjectId("57b67014333a9a6dcc38bb7f"),
+      ObjectId("57b67010333a9a6dcc38bb7e"),
+      ObjectId("57b6700b333a9a6dcc38bb7d"),
+      ObjectId("57b67006333a9a6dcc38bb7c"),
+      ObjectId("57b67001333a9a6dcc38bb7b"),
+      ObjectId("57b66ffd333a9a6dcc38bb7a")
+    ]
   },
 )
 
@@ -72,3 +104,7 @@ surveys = (
   db.surveys.insert({ brandName: "Giant", brandUsage: 4, npsScore: 7, npsReason: 2, user: ObjectId("57b66c31333a9a6dcc38bb67")})
   db.surveys.insert({ brandName: "Trump", brandUsage: 4, npsScore: 2, npsReason: 1, user: ObjectId("57b66c31333a9a6dcc38bb67")})
 )
+
+
+
+db.users.update({id:ObjectId("57b66a67333a9a6dcc38bb65")} ,{firstName: "Interesting",     lastName: "Name",     email: "name@test.com",     password: "123456",     gender: 1,     age: 1,     ethnicity: 1,     householdIncome: 1,politicalParty: 1, timeOnline: 1, })
