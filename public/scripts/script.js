8c6b27a9c6b41c957cfdf5661b837ef465d20d71
meeniebeanie <minyen.j@gmail.com>
2016-08-21 19:04:27 +0800

    finished with static pages- left with about us

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,4 +2,11 @@ $(function() {
 
   $("#brandname").fadeIn(4000);
 
+
+  $("#arrow").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#navigate").offset().top},
+          'slow');
+  });
+
 });
