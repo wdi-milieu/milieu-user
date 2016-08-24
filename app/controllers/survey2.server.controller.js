e5307ebf04c1ad16640f84b70b0e31afe1fa9124
meeniebeanie <minyen.j@gmail.com>
2016-08-24 12:12:25 +0800

    rendered api data for all 6 questions

--- app/controllers/survey2.server.controller.js
+++ app/controllers/survey2.server.controller.js
@@ -2,14 +2,92 @@
 var survey_arr2 = require('../../public/scripts/survey2');
 var Survey = require('mongoose').model('ClassSurvey');
 
+var question1, question2, question3, question4, question5, question6;
+
+Survey.aggregate([
+  { $group: {
+    _id: "$questionOne",
+    total: { $sum: 1}
+    }
+  },
+  { $sort: { '_id': 1}}
+],function(err,result){
+  question1 = result;
+});
+
+
+Survey.aggregate([
+  { $group: {
+    _id: "$questionTwo",
+    total: { $sum: 1}
+    }
+  },
+  { $sort: { '_id': 1}}
+],function(err,result){
+  question2 = result;
+});
+
+Survey.aggregate([
+  { $group: {
+    _id: "$questionThree",
+    total: { $sum: 1}
+    }
+  },
+  { $sort: { '_id': 1}}
+],function(err,result){
+  question3 = result;
+});
+
+Survey.aggregate([
+  { $group: {
+    _id: "$questionFour",
+    total: { $sum: 1}
+    }
+  },
+  { $sort: { '_id': 1}}
+],function(err,result){
+  question4 = result;
+});
+
+Survey.aggregate([
+  { $group: {
+    _id: "$questionFive",
+    total: { $sum: 1}
+    }
+  },
+  { $sort: { '_id': 1}}
+],function(err,result){
+  question5 = result;
+});
+
+Survey.aggregate([
+  { $group: {
+    _id: "$questionSix",
+    total: { $sum: 1}
+    }
+  },
+  { $sort: { '_id': 1}}
+],function(err,result){
+  question6 = result;
+});
+
 module.exports = {
+
+  index: function(req, res){
+    //this is for api export & json things
+    // Survey.find({}, function(err, survey){
+    //   if (err) res.status(400).send(err);
+    //   res.json(survey);
+    // });
+    res.json({question1, question2, question3, question4, question5, question6 });
+  }, //close index function
 
   new: function(req, res){
     res.render('surveys/take2', {
       title: 'WDI4 Survey',
       survey: survey_arr2
     });
-  }, //new function closing
+  }, // close new function
 
   create: function(req, res, next){
     var survey = new Survey(req.body);
@@ -18,6 +96,6 @@ module.exports = {
       console.log('save successful');
       res.redirect('/');
     });
-  }
+  } // close create function
 
 }; //module exports closing bracket
