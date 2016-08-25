09c5a77f5aaee9f5f36f542de5661b6ea433a46a
Gerald Ang <geraldangw@gmail.com>
2016-08-25 10:33:55 +0800

    test

--- app/controllers/survey2.server.controller.js
+++ app/controllers/survey2.server.controller.js
@@ -190,31 +190,38 @@ module.exports = {
   index: function(req, res){
     //this is for api export & json things
 
+    var q1 = question1[0];
+    var q2 = question2[0];
+    var q3 = question3[0];
+    var q4 = question4[0];
+    var q5 = question5[0];
+    var q6 = question6[0];
+
     var classSurveyResult = {};
 
-        classSurveyResult['q1opt1'] = parseInt((question1[0].opt1));
-        classSurveyResult['q1opt2'] = parseInt((question1[0].opt2));
+        classSurveyResult['q1opt1'] = parseInt(q1.opt1);
+        classSurveyResult['q1opt2'] = parseInt(q1.opt2);
 
-        classSurveyResult['q2opt1'] = parseInt((question2[0].opt1));
-        classSurveyResult['q2opt2'] = parseInt((question2[0].opt2));
-        classSurveyResult['q2opt3'] = parseInt((question2[0].opt3));
+        classSurveyResult['q2opt1'] = parseInt(q2.opt1);
+        classSurveyResult['q2opt2'] = parseInt(q2.opt2);
+        classSurveyResult['q2opt3'] = parseInt(q2.opt3);
 
-        classSurveyResult['q3opt1'] = parseInt((question3[0].opt1));
-        classSurveyResult['q3opt2'] = parseInt((question3[0].opt2));
-        classSurveyResult['q3opt3'] = parseInt((question3[0].opt3));
+        classSurveyResult['q3opt1'] = parseInt(q3.opt1);
+        classSurveyResult['q3opt2'] = parseInt(q3.opt2);
+        classSurveyResult['q3opt3'] = parseInt(q3.opt3);
 
-        classSurveyResult['q4opt1'] = parseInt((question4[0].opt1));
-        classSurveyResult['q4opt2'] = parseInt((question4[0].opt2));
-        classSurveyResult['q4opt3'] = parseInt((question4[0].opt3));
+        classSurveyResult['q4opt1'] = parseInt(q4.opt1);
+        classSurveyResult['q4opt2'] = parseInt(q4.opt2);
+        classSurveyResult['q4opt3'] = parseInt(q4.opt3);
 
-        classSurveyResult['q5opt1'] = parseInt((question5[0].opt1));
-        classSurveyResult['q5opt2'] = parseInt((question5[0].opt2));
+        classSurveyResult['q5opt1'] = parseInt(q5.opt1);
+        classSurveyResult['q5opt2'] = parseInt(q5.opt2);
 
 
-        classSurveyResult['q6opt1'] = parseInt((question6[0].opt1));
-        classSurveyResult['q6opt2'] = parseInt((question6[0].opt2));
-        classSurveyResult['q6opt3'] = parseInt((question6[0].opt3));
-        classSurveyResult['q6opt4'] = parseInt((question6[0].opt4));
+        classSurveyResult['q6opt1'] = parseInt(q6.opt1);
+        classSurveyResult['q6opt2'] = parseInt(q6.opt2);
+        classSurveyResult['q6opt3'] = parseInt(q6.opt3);
+        classSurveyResult['q6opt4'] = parseInt(q6.opt4);
 
 
     res.json(classSurveyResult);
